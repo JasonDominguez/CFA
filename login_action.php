c162b03def11382bdc28a1f6c504bb9d79146f2c
jdomingu <jdomi011@odu.edu>
2019-11-27 19:57:47 -0500

    login should work with password hash

--- login_action.php
+++ login_action.php
@@ -1,8 +1,6 @@
 <?php
   if(session_id() == '' || !isset($_SESSION)) {
-    // session isn't started
     session_start();
-    $_SESSION['message'] = "session was started on action page";
   }
 
   if (empty($_POST['userid']) || empty($_POST['password']) ) {
@@ -11,31 +9,29 @@
     return;
   }
 
-    // check credentials
-    //  if ($_POST['userid'] == $user['userid'] && $user['password'] ==
-    //      password_hash($_POST['password'], PASSWORD_BCRYPT, ['salt' => $user['salt']]) ) {
-
     $db = new SQLite3('cfa.db') or die('Unable to open database');
-    
-    $user = sanitize($_POST['userid']);
-    $pass = sanitize($_POST['password']);
+    $userid = $_POST['userid'];
+    $password = $_POST['password'];
+    $query = <<<QUERY
+    SELECT *
+    FROM users
+    WHERE userId = '$userid'
+QUERY;
 
-    $result = $db->query("SELECT * FROM users WHERE userId = $user;") or die('Query failed');
-
-    while ($row = $result->fetchArray())
-    {
-        $_SESSION['message'] = "User: {$row['userId']}\nFirst name: {$row['fname']}\n";
-    } 
+    $result = $db->query($query) or die('Query failed');
+    while($row = $result->fetchArray()){$data = $row;}
     $db->close();
+   
+    if ($data && password_verify($data['password'], $password)){
+        $_SESSION['userid'] = $_POST['userid'];
+        $_SESSION['logged_in'] = true;
+        $_SESSION['message'] = $_SESSION['userid'] . " logged in";
+        require('index.php');
+    }
+    else{
+        $_SESSION['message'] = "not logged in";
+    }
     require('login.php');
-
-//   if ($_POST['userid'] == $user['userid'] && password_verify($_POST['password'], $user['password'])) {
-
-//     $_SESSION['userid'] = $_POST['userid'];
-//     $_SESSION['logged_in'] = true;
-//     $_SESSION['message'] = $_SESSION['userid'] . " logged in";
-//     require('index.php');
-//   }
 
 //   else {
 //     $_SESSION['message'] = "Invalid credentials - please try again";
